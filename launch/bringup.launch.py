f663f49c493f41e7dbcbd330d114a896ffd917d4
Michael Hart <michael@modulr.cloud>
2025-12-24 17:03:43 +0000

    Rename package in all launch nodes

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         # Forward motion from /cmd_vel to /wirelesscontroller
         Node(
-            package='modulr_unitree',
+            package='modulr_unitree_go2',
             executable='fwd_motion',
             name='fwd_motion_node',
             output='screen',
@@ -13,7 +13,7 @@ def generate_launch_description():
 
         # Forward images from SDK node to /camera/image_raw
         Node(
-            package='modulr_unitree',
+            package='modulr_unitree_go2',
             executable='fwd_camera',
             name='fwd_camera_node',
             output='screen',
@@ -21,7 +21,7 @@ def generate_launch_description():
 
         # Capture images from SDK and send over socket
         Node(
-            package='modulr_unitree',
+            package='modulr_unitree_go2',
             executable='unitree_video_zmq',  # replace with your executable name
             name='unitree_video_zmq_node',
             output='screen',
