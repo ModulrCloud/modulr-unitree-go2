2795953789e47fccf95256bf99a6384026ed665e
rickford1 <rickfriedman@vt.edu>
2026-03-18 14:01:34 -0400

    added queue size to clear previous buffer

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -87,6 +87,7 @@ def generate_launch_description():
             parameters=[{
                 'target_frame': 'utlidar_lidar',
                 'transform_tolerance': 0.01,
+                'queue_size': 1,
                 'min_height': -0.5,
                 'max_height': 0.5,
                 'angle_min': -3.14159,
